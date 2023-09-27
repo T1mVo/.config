755116a15842c7c7f94e83aa44d5e26d2b596ee2
Tim Voßhenrich <94936637+T1mVo@users.noreply.github.com>
2023-09-27 10:36:25 +0200

    Add fish config files

---
+++ fish/alias.fish
@@ -0,0 +1,7 @@
+# Git
+alias gst 'git status'
+alias ggr 'git log --graph'
+
+# Python
+alias py3 python3
+alias py python3
---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+# Syntax highlighting
+source ~/.config/fish/syntax.fish
+
+# Aliases
+source ~/.config/fish/alias.fish
+
+# Starship prompt initialization
+starship init fish | source
+
+# pyenv initialization
+pyenv init - | source
