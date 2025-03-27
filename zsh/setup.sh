65eba2ed6312c90b0769c418c4e49a7050ff395c
Tim Voßhenrich <94936637+T1mVo@users.noreply.github.com>
2025-03-27 03:59:50 +0100

    Modified zsh config files

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,6 +1,10 @@
-ln -s .zshenv "$HOME/.zshenv"
+#!/bin/sh
+
+ln -s ".config/zsh/.zshenv" "$HOME/.zshenv"
 
 export ZDOTDIR="$HOME/.config/zsh"
 export ZSH="$ZDOTDIR/.oh-my-zsh"
+export RUNZSH="no"
+export KEEP_ZSHRC="yes"
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
